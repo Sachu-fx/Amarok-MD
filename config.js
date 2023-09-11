8bcc4b639d529e7c06e3bb0c584b0878414e8745
Sachu <112187416+Sachu-fx@users.noreply.github.com>
2023-09-11 20:47:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ if (!gg) {
 
 global.owner = gg.split(",");
 global.mongodb = process.env.MONGODB || "mongodb+srv://amarok:124357cluster@cluster0.axfdbig.mongodb.net/?retryWrites=true&w=majority";
-global.sessionId = process.env.SESSION_ID || "";
+global.sessionId = process.env.SESSION_ID || "abcdefghijklmnopqrstuvwxyz";
 global.prefa = process.env.PREFIX || "-";
 global.tenorApiKey =
   process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
